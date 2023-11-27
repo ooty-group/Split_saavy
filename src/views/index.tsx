f5bc5cc2bc07efe84fd5a018f68f9cf19e0bf269
Bradeep <bradeep001@gmail.com>
2023-11-28 01:15:36 +0530

    Index file

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import styles from "./styles.module.scss";
+import Button from "../components/Button";
 
 const SplitSaavy = () => {
-  return <div>Split Saavy</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.titleBar}>Split Saavy</div>
+      <div className={styles.container}>
+        <Button
+          onClick={() => {
+            console.log("Hi");
+          }}
+          label="Click Me!"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default SplitSaavy;
